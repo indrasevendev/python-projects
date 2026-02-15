cd82f4355a55a5d3f546024ef83eb66eb5fde7c7
indrasevendev <indrasevendev@gmail.com>
2026-02-15 17:52:12 +0700

    Different message in feature branch

--- mini_games/tebak_angka.py
+++ mini_games/tebak_angka.py
@@ -4,6 +4,6 @@ angka = random.randint(1, 10)
 tebakan = int(input("Tebak angka 1-10: "))
 
 if tebakan == angka:
-    print("Benar!")
+    print("Kamu menebak dengan tepat!")
 else:
     print("Salah! Jawabannya:", angka)
