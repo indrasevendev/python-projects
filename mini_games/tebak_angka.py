c7aab75a45a200f78eab2942d9d0292dd2f8fd51
indrasevendev <indrasevendev@gmail.com>
2026-02-15 17:50:51 +0700

    Update message in main branch

--- mini_games/tebak_angka.py
+++ mini_games/tebak_angka.py
@@ -4,6 +4,6 @@ angka = random.randint(1, 10)
 tebakan = int(input("Tebak angka 1-10: "))
 
 if tebakan == angka:
-    print("Benar!")
+    print("Selamat! Kamu benar!")
 else:
     print("Salah! Jawabannya:", angka)
